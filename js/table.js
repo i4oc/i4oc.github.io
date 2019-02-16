b7bdfd057bb2014e7cf2f437c62d242aceabfbf3
Silvio Peroni <essepuntato@gmail.com>
2019-02-16 15:10:00 +0100

    New list of publishers

--- js/table.js
+++ js/table.js
@@ -1,12 +1,11 @@
 /*
  * The data exposed via this script are taken from
  * https://www.crossref.org/reports/members-with-open-references/.
- * It is important to clean the aforementioned CSV with the script
- * "remove_empty.py so as to include only the publishers that actually
- * comply with the release of open references."
+ * The logic for including/excluding publishers has been directly
+ * embedded in this scripts.
  */
 
-function get_csv(pub_path, rem_path, lnk_path) {
+function get_csv(pub_path, rem_path) {
     $.ajax({
         url : pub_path,
         dataType: "text",
@@ -15,32 +14,18 @@ function get_csv(pub_path, rem_path, lnk_path) {
                 url : rem_path,
                 dataType: "text",
                 success : function (rem_data) {
-                    $.ajax({
-                        url : lnk_path,
-                        dataType: "text",
-                        success : function (lnk_data) {
-                            create_table(pub_data, rem_data, lnk_data);
-                        }
-                    });
+                    create_table(pub_data, rem_data);
                 }
             });
         }
     });
 }
 
-function create_table(pub_data, rem_data, lnk_data) {
+function create_table(pub_data, rem_data) {
     var table = $("#partpublishers");
 
     var json_data = $.csv.toObjects(pub_data);
-    var json_link = $.csv.toObjects(lnk_data);
     var json_remo = $.csv.toObjects(rem_data);
-
-    /* Substituted with the link to Crossref Member Participation page
-    var link_dict = {};
-    json_link.forEach(function(entry) {
-        link_dict[entry["Publisher"]] = entry["Address"];
-    });
-    */
 
     var remo_array = [];
     json_remo.forEach(function(entry) {
@@ -56,31 +41,34 @@ function create_table(pub_data, rem_data, lnk_data) {
             new_row = false;
         }
 
+        /* The name of the publisher */
         var cur_entry_text = entry["Member Name & ID"];
 
+        /* The publisher will be considered only if it is not included in the remove list */
         if ($.inArray(cur_entry_text, remo_array) != 0) {
-            var pub_name = cur_entry_text.substring(0, cur_entry_text.indexOf(' (ID'));
-            var pub_id = cur_entry_text.substring(
-                cur_entry_text.indexOf(' (ID ') + 5, cur_entry_text.length - 1);
+            /* Other constraints to check */
+            var ref_visibility = entry["Reference Visibility"]
+            var back_doi = parseInt(entry["Total Backfile DOIs"])
+            var back_deposit = entry["Deposits Backfile References"]
+            var current_doi = parseInt(entry["Total Current DOIs"])
+            var current_deposit = entry["Deposits Current References"]
 
-            if ($.inArray(pub_name, done) != 0) {
-                i = (i + 1) % 2;
-                new_row = true;
-                done.push(pub_name);
+            if (    ref_visibility == "open" &&
+                    current_deposit == "true" &&
+                    (current_doi > 0 || (backfile_doi > 0 && back_deposit == "true"))) {
+                var pub_name = cur_entry_text.substring(0, cur_entry_text.indexOf(' (ID'));
+                var pub_id = cur_entry_text.substring(
+                    cur_entry_text.indexOf(' (ID ') + 5, cur_entry_text.length - 1);
 
-                /*
-                if (pub_name in link_dict) {
-                    var cur_link = link_dict[pub_name];
-                    if (cur_link != "None") {
-                        pub_name = "<a href='" + link_dict[pub_name] + "'>" + pub_name + "</a>";
-                    }
+                if ($.inArray(pub_name, done) == -1) {
+                    i = (i + 1) % 2;
+                    new_row = true;
+                    done.push(pub_name);
+                    pub_name = "<a href='https://www.crossref.org/members/prep/" +
+                        pub_id + "'>" + pub_name + "</a>";
+
+                    $("#partpublishers tr:last-child").append("<td>" + pub_name + "</td>");
                 }
-                */
-
-                pub_name = "<a href='https://www.crossref.org/members/prep/" +
-                    pub_id + "'>" + pub_name + "</a>";
-
-                $("#partpublishers tr:last-child").append("<td>" + pub_name + "</td>");
             }
         }
     });
@@ -89,7 +77,6 @@ function create_table(pub_data, rem_data, lnk_data) {
 var data_dir = "data/";
 var pub_file = data_dir + "crossref.txt";
 var rem_file = data_dir + "remove.txt";
-var lnk_file = data_dir + "links.txt";
 
 var table = $("#partpublishers");
-get_csv(pub_file, rem_file, lnk_file);
+get_csv(pub_file, rem_file);
